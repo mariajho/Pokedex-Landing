ffb939e5c631f3e4826695b405d716439af4b4d8
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-07-07 17:05:31 -0400

    avanzando menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,31 @@ $(document).ready(function($){
 		console.log("complete");
 	});
 });
+
+
+
+'use strict';
+
+$(function(){
+  //If the overlay is clicked, then we close the menu bars.
+  $('.overlay').on('click', function(){
+    $(this).fadeOut(300);
+    $('.menu-bar').removeClass('is-active');
+    $('.menu-button').removeClass('is-clicked');
+  });
+  
+  $('.menu-button').on('click', function(){
+    var side = $(this).data('menu');
+    //If this has is-clicked, then we close the menu.
+    if($(this).hasClass('is-clicked')){
+      //Since we created the code for overlay already.
+      $('.overlay').click();
+    }
+    //Otherwise, we open it.
+    else{
+      $('.overlay').fadeIn(300);
+      $(this).addClass('is-clicked');
+      $('.menu-bar.'+side).addClass('is-active');
+    }
+  });
+});
